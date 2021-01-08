8bd00eeeed6d14713a96d4bd3a93e1c4fbffe31a
ChenYn <chenyn01@fenbi.com>
2021-01-08 14:26:56 +0800

    Delete Snippets

--- Snippets/README.md
+++
@@ -1,3 +0,0 @@
-# Snippets
-
-- [你应该了解的 25 个 JS 技巧](https://mp.weixin.qq.com/s/xtQbPTW6iDqHH-muu_Vlcg)
--- Snippets/Random.ts
+++
@@ -1,6 +0,0 @@
-/*
- * 随机数
- */
-function random(max: number = 1, min: number = 0) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
