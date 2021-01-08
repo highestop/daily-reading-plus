fe1bcd751d99a945a0fabafd2745b64eec281a1b
Nicky Chen <highestopchen@gmail.com>
2021-01-08 11:56:02 +0800

    Create isCollectionEmpty.js

---
+++ JavaScript/isCollectionEmpty.js
@@ -0,0 +1,21 @@
+/*
+ * 判断一个集合（ 数组、对象、Set、Map ）列表
+ */
+function isEmpty(x) {
+  if(Array.isArray(x)
+    || typeof x === 'string'
+    || x instanceof String
+   ) {
+    return x.length === 0;
+  }
+
+  if(x instanceof Map || x instanceof Set) {
+    return x.size === 0;
+  }
+
+  if(({}).toString.call(x) === '[object Object]') {
+    return Object.keys(x).length === 0;
+  }
+
+  return false;
+}
